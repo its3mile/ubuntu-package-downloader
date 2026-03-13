23855e834c3f1d3ffef855febd4e97415f9e7e3e
Emile Boutany <emile.boutany@daintta.com>
2026-03-13 09:32:29 +0000

    feat: use click instead of argparse
    This allows for more complex commands and chaining

--- src/ubuntu_package_downloader/__init__.py
+++ src/ubuntu_package_downloader/__init__.py
@@ -1,5 +1,6 @@
-import argparse
 import sys
+
+import click
 from loguru import logger
 
 from .config import Settings
@@ -9,71 +10,50 @@ SUCCESS = 0
 ERROR = 1
 
 
-def main():
+@click.command("download")
+@click.argument("name")
+@click.option(
+    "-p",
+    "--package-version",
+    default="latest",
+    help="Specify the version of the package to download",
+)
+@click.option(
+    "-a",
+    "--architecture",
+    default="amd64",
+    help="Specify the architecture of the package to download",
+)
+@click.option(
+    "-d",
+    "--distribution-series",
+    default="24.04",
+    help="The edition of the package to download i.e., 24.04, 23.10, focal, noble, etc.",
+)  # default to latest LTS at time of writing
+@click.option("--depth", default=0, type=int, help="Recursion depth")
+@click.option(
+    "--verbose",
+    is_flag=True,
+    help="Enable verbose logging, which will print more detailed logs to the console",
+)
+def main(
+    name: str,
+    package_version: str,
+    architecture: str,
+    distribution_series: str,
+    depth: int,
+    verbose: bool,
+):
     """
-    Process command line arguments and call ubuntu_package_downloader function
+    Ubuntu Package Downloader (UPD) is a command-line utility that allows users to download packages from the Ubuntu repositories, including their dependencies. It provides options to specify the package version, architecture, distribution series, and whether to include dependencies in the download process. The tool is designed to be flexible and user-friendly, making it easier for users to manage their package downloads effectively.
     """
     # load settings
     settings = Settings()
 
-    parser = argparse.ArgumentParser(
-        prog=settings.project.name, description=settings.project.description
-    )
-    parser.add_argument(
-        "-v",
-        "--version",
-        action="version",
-        version=f"{settings.project.name} {settings.project.version}",
-    )
-    parser.add_argument("name", type=str, help="Package to download")
-    parser.add_argument(
-        "-p",
-        "--package-version",
-        type=str,
-        help="Specify the version of the package to download",
-        default="latest",
-    )
-    parser.add_argument(
-        "-d",
-        "--distribution-series",
-        type=str,
-        help="The edition of the package to download i.e., 24.04, 23.10, focal, noble, etc.",
-        default="24.04",  # default to latest LTS at time of writing
-    )
-    parser.add_argument(
-        "-a",
-        "--architecture",
-        type=str,
-        help="Specify the architecture of the package to download",
-        default="amd64",
-    )
-    parser.add_argument(
-        "-w",
-        "--with-dependencies",
-        action="store_true",
-        help="Recursively download dependencies",
-    )
-    parser.add_argument(
-        "--depth",
-        type=int,
-        default=1,
-        required=(
-            "-w" in sys.argv and "--depth" in sys.argv
-        ),  # only required if -w specified, and user specified it, so that default can be used
-        help="Set the dependency recursion depth, defaults to 1",
-    )
-    parser.add_argument(
-        "--verbose",
-        action="store_true",
-        help="Enable verbose logging, which will print more detailed logs to the console",
-    )
-    # parse arguments
-    args = parser.parse_args()
-
     # replace default logger with new logger with level based on verbose flag
-    logger.remove() 
-    logger.add(sys.stderr, level="DEBUG" if args.verbose else "INFO") 
-    
+    logger.remove()
+    logger.add(sys.stderr, level="DEBUG" if verbose else "INFO")
+
     # get UbuntuPackageDownloader instance
     upd = UbuntuPackageDownloader(
         settings.launchpad.consumer_name,
@@ -83,16 +63,16 @@ def main():
     )
 
     # set recursion limit
-    upd.recursion_limit = args.depth
+    upd.recursion_limit = depth
 
     return (
         sys.exit(SUCCESS)
         if upd.download(
-            package_name=args.name,
-            package_version=args.package_version,
-            distribution_series=args.distribution_series,
-            architecture=args.architecture,
-            with_dependencies=args.with_dependencies,
+            package_name=name,
+            package_version=package_version,
+            distribution_series=distribution_series,
+            architecture=architecture,
+            with_dependencies=True if depth > 0 else False,
         )
         else sys.exit(ERROR)
     )
